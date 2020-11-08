f38e3f21aca2521235ce9573cfe8062746e5de43
Seaborn Lee <seabornlee.cn@gmail.com>
2020-11-08 08:29:08 +0800

    employers_should_only_be_able_to_see_jobs_published_by_them

--- test/ApplicationTest.cpp
+++ test/ApplicationTest.cpp
@@ -5,7 +5,6 @@ extern "C" {
 #include <linked_list.h>
 }
 
-
 TEST(ApplicationTest, employers_should_be_able_to_publish_a_job) {
     char *employerName = "";
     char *jobName = "高级前端开发";
@@ -16,4 +15,19 @@ TEST(ApplicationTest, employers_should_be_able_to_publish_a_job) {
 
     ASSERT_STREQ("高级前端开发", (char *) getItem(job, 0));
     ASSERT_STREQ("JReq", (char *) getItem(job, 1));
+}
+
+TEST(ApplicationTest, employers_should_only_be_able_to_see_jobs_published_by_them) {
+    char *employerAlibaba = "Alibaba";
+    char *employerTencent = "Tencent";
+    char *seniorJavaDevJob = "高级Java开发";
+    char *juniorJavaDevJob = "Java开发";
+
+    Application *pApplication = newApplication();
+    execute(pApplication, "publish", employerAlibaba, seniorJavaDevJob, "JReq", NULL, NULL, 0L);
+    execute(pApplication, "publish", employerTencent, juniorJavaDevJob, "JReq", NULL, NULL, 0L);
+    LinkedList *jobs = getJobs(pApplication, employerAlibaba, "published");
+    ASSERT_EQ(1, len(jobs));
+    LinkedList *pList = (LinkedList *) getItem(jobs, 0);
+    ASSERT_STREQ("高级Java开发", (char *) getItem(pList, 0));
 }
